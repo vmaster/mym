a05e9d85629d2d0a6acb75f85b51e917f1ad78b2
Alan Hugo <alan_hugo@outlook.com>
2017-10-25 07:01:22 -0500

    Crear tarea de un día anterior automáticamente

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -77,4 +77,61 @@ class AppController extends Controller {
        		 }
 		}
 	}
+
+	public function supervisores_enosa(){
+		$this->loadModel('User');
+		$arr_obj_usuario = $this->User->find('all',array(
+    			'fields' => array('User.id'),
+    			'conditions'=>array(
+                        'User.consorcio_id' => 2,
+                        'User.estado' => 1
+    			)
+    		)
+    	);
+    	//debug($arr_obj_usuario);
+    	return $arr_obj_usuario;
+	}
+
+	public function tareas_supervisores_del_dia_ayer($user_id){
+		$this->loadModel('Tarea');
+		$arr_obj_tareas = $this->Tarea->find('all',array(    			
+    			'conditions'=>array(
+                        'Tarea.user_id' => $user_id,
+                        'Tarea.created = DATE_SUB(CURDATE(), INTERVAL 1 DAY)'
+    			)
+    		)
+    	);
+    	//debug($arr_obj_tareas);exit();
+    	return $arr_obj_tareas;
+	}
+
+	/**
+	 * Creamos tareas del dia anterior al supervisor enosa
+	 * @return boolean
+	 * @author Alan Hugo
+	 * @version 25 Octubre 2017
+	 */
+	public function crear_tarea_supervisor_dia_anterior(){
+		$this->loadModel('Tarea');
+		$arr_obj_usuarios = $this->supervisores_enosa();
+
+		foreach ($arr_obj_usuarios as $key => $obj_usuario) {
+			$user_id = $obj_usuario['User']['id'];
+			$arr_obj_tareas = $this->tareas_supervisores_del_dia_ayer($user_id);
+
+			if(empty($arr_obj_tareas)){
+				$arr_obj_tarea['user_id'] = $user_id;
+				$arr_obj_tarea['created'] = date('Y-m-d', (strtotime(date('Y-m-d')) - 3600));
+				$arr_obj_tarea['modified'] = $arr_obj_tarea['created'];
+				$arr_obj_tarea['estado'] = '0';
+		    	
+		    	$this->Tarea->create();
+				$this->Tarea->save($arr_obj_tarea);
+			}
+		}
+
+		return true;
+	}
+
+
 }
